bd3209dbf859d50867af22e2be4a91ecd300c84f
Anton Bauer <anton.bauer2@generic.de>
2024-10-28 14:07:10 +0100

    [Tesseract] implement basic extraction

--- src/ReceiptsRecognition.Domain/Library.fs
+++ src/ReceiptsRecognition.Domain/Library.fs
@@ -1,5 +1,14 @@
 ﻿namespace ReceiptsRecognition.Domain
 
+open Tesseract
+
 module Say =
-    let hello name =
-        printfn "Hello %s" name
+    let hello filePath =
+        let engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default)
+        let image = Pix.LoadFromFile(filePath)
+        let text = engine.Process(image)
+
+        image.Dispose();
+        engine.Dispose()
+
+        text
